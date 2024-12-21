ba8920ec41df937f0e2b63cde9066c1aeed04432
FriedAxons <friedaxons@proton.me>
2024-12-21 13:01:30 -0600

    Initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Includes all relevant files in your src directory
+  ],
+  theme: {
+    extend: {
+      colors: {},
+      fontFamily: {},
+    },
+  },
+  plugins: [],
+};
