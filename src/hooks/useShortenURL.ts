377e77f0e82794acc4b4c0644c6f649614d9628b
FriedAxons <friedaxons@proton.me>
2024-12-21 13:41:24 -0600

    Implement hook file for URL shortening
    Uses cleanuri API.

---
+++ src/hooks/useShortenURL.ts
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import axios from "axios";
+
+const API_URL = "https://cleanuri.com/api/v1/shorten";
+// Initialize shortenedLinks as an array of objects
+export const useShortenURL = () => {
+  const [shortenedLinks, setShortenedLinks] = useState<
+    {
+      original: string;
+      short: string;
+    }[]
+  >([]); // Empty array as the initial state
+
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const shortenURL = async (url: string) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await axios.post(API_URL, { url });
+      setShortenedLinks((prev) => [
+        ...prev,
+        { original: url, short: response.data.result_url },
+      ]);
+    } catch {
+      setError("Failed to shorten the URL. Please try again.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { shortenedLinks, error, loading, shortenURL };
+};
