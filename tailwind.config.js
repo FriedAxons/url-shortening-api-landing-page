9e873f9357837db80ed34a404ac69f7d660b41fc
FriedAxons <friedaxons@proton.me>
2024-12-21 21:51:59 -0600

    Add custom colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,15 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {},
-      fontFamily: {},
+      colors: {
+        gray: "hsl(0, 0%, 75%)",
+        grayishviolet: "hsl(257, 7%, 63%)",
+        darkblue: "hsl(255, 11%, 22%)",
+        darkviolet: "hsl(260, 8%, 14%)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
     },
   },
   plugins: [],
