2427b973fbf15f09f4b9d45ae145d3c472bc11ab
FriedAxons <friedaxons@proton.me>
2024-12-26 22:35:24 -0600

    Style InfoSection component

--- frontend/src/components/InfoSection.tsx
+++ frontend/src/components/InfoSection.tsx
@@ -61,44 +61,65 @@ const InfoSection = () => {
   const DesktopInfoSection = () => (
     <div className="lg:xy-p mxy-p">
       <div className="flex flex-col items-center">
-        <h1 className="text-4xl font-bold text-verydarkviolet">
+        <h1 className="text-4xl font-bold text-verydarkviolet mb-4">
           Advanced Statistics
         </h1>
-        <p className="text-grayishviolet font-medium">
+        <p className="w-[29%] leading-7  text-center text-grayishviolet font-medium">
           Track how your links are performing across the web with our advanced
           statistics dashboard.
         </p>
       </div>
-      <div className="flex flex-row">
-        <div>
-          <img
-            src="./svgs/icon-brand-recognition.svg"
-            alt="Brand Recognition Illustration"
-          />
-          <h2>Brand Recognition</h2>
-          <p>
+
+      <div className="relative flex flex-row justify-center">
+        <div className="max-h-[320px] w-[380px] bg-white p-6 rounded-lg shadow-lg">
+          <div className="flex justify-center items-center bg-darkviolet w-20 h-20 mb-8 rounded-full">
+            <img
+              src="./svgs/icon-brand-recognition.svg"
+              alt="Brand Recognition Illustration"
+            />
+          </div>
+          <h2 className="text-verydarkviolet text-xl font-bold mb-4">
+            Brand Recognition
+          </h2>
+          <p className="text-grayishviolet">
             Boost your brand recognition with each click. Generic links don't
             mean a thing. Branded links help instill confidence in your content.
           </p>
         </div>
-        <div>
-          <img
-            src="./svgs/icon-detailed-records.svg"
-            alt="Detailed Records Illustration"
-          />
-          <h2>Detailed Records</h2>
-          <p>
+
+        {/* Cyan Bar */}
+        <div className="w-16 h-2 bg-cyan self-center transform translate-y-12"></div>
+
+        <div className="max-h-[320px] w-[380px] bg-white p-6 rounded-lg shadow-lg mt-12">
+          <div className="flex justify-center items-center bg-darkviolet w-20 h-20 mb-8 rounded-full">
+            <img
+              src="./svgs/icon-detailed-records.svg"
+              alt="Brand Recognition Illustration"
+            />
+          </div>
+          <h2 className="text-verydarkviolet text-xl font-bold mb-4">
+            Detailed Records
+          </h2>
+          <p className="text-grayishviolet">
             Gain insights into who is clicking your links. Knowing when and
             where people engage with your content helps inform better decisions.
           </p>
         </div>
-        <div>
-          <img
-            src="./svgs/icon-fully-customizable.svg"
-            alt="Fully Customizable Illustration"
-          />
-          <h2>Fully Customizable</h2>
-          <p>
+
+        {/* Cyan Bar */}
+        <div className="w-16 h-2 bg-cyan self-center transform translate-y-12"></div>
+
+        <div className="max-h-[320px] w-[380px] bg-white p-6 rounded-lg shadow-lg mt-24">
+          <div className="flex justify-center items-center bg-darkviolet w-20 h-20 mb-8 rounded-full">
+            <img
+              src="./svgs/icon-fully-customizable.svg"
+              alt="Brand Recognition Illustration"
+            />
+          </div>
+          <h2 className="text-verydarkviolet text-xl font-bold mb-4">
+            Fully Customizable
+          </h2>
+          <p className="text-grayishviolet">
             Improve brand awareness and content discoverability through
             customizable links, supercharging audience engagement.
           </p>
