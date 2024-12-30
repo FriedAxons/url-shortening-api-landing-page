6f2310bd11add8b1bcf26dca60deadbaec96b920
FriedAxons <friedaxons@proton.me>
2024-12-29 21:36:07 -0600

    Style InfoSection component

--- frontend/src/components/InfoSection.tsx
+++ frontend/src/components/InfoSection.tsx
@@ -5,7 +5,7 @@ const InfoSection = () => {
 
   const MobileInfoSection = () => (
     <div className="lg:xy-p mxy-p">
-      <div className="flex flex-col items-center text-center">
+      <div className="flex flex-col items-center text-center mb-12">
         <h1 className="text-3xl font-bold text-verydarkviolet mb-5">
           Advanced Statistics
         </h1>
@@ -14,42 +14,57 @@ const InfoSection = () => {
           statistics dashboard.
         </p>
       </div>
-      <div className="flex flex-col">
+
+      <div className="flex flex-col items-center pb-24">
         <div className="flex flex-col items-center text-center">
-          <img
-            src="./svgs/icon-brand-recognition.svg"
-            alt="Brand Recognition Illustration"
-          />
-          <h2 className="text-xl font-bold text-verydarkviolet mb-5">
+          <div className="flex justify-center items-center bg-darkviolet w-20 h-20 mb-8 rounded-full">
+            <img
+              src="./svgs/icon-brand-recognition.svg"
+              alt="Brand Recognition Illustration"
+            />
+          </div>
+          <h2 className="text-2xl font-bold text-verydarkviolet mb-5">
             Brand Recognition
           </h2>
-          <p className="text-grayishviolet font-medium">
+          <p className="w-[86%] text-grayishviolet font-medium mb-10">
             Boost your brand recognition with each click. Generic links don't
             mean a thing. Branded links help instill confidence in your content.
           </p>
         </div>
+
+        {/* Cyan Bar (vertical for mobile) */}
+        <div className="w-2 h-12 bg-cyan"></div>
+
         <div className="flex flex-col items-center text-center">
-          <img
-            src="./svgs/icon-detailed-records.svg"
-            alt="Detailed Records Illustration"
-          />
-          <h2 className="text-xl font-bold text-verydarkviolet mb-5">
+          <div className="flex justify-center items-center bg-darkviolet w-20 h-20 mb-8 rounded-full">
+            <img
+              src="./svgs/icon-detailed-records.svg"
+              alt="Detailed Records Illustration"
+            />
+          </div>
+          <h2 className="text-2xl font-bold text-verydarkviolet mb-5">
             Detailed Records
           </h2>
-          <p className="text-grayishviolet font-medium">
+          <p className="text-grayishviolet font-medium mb-10">
             Gain insights into who is clicking your links. Knowing when and
             where people engage with your content helps inform better decisions.
           </p>
         </div>
+
+        {/* Cyan Bar (vertical for mobile) */}
+        <div className="w-2 h-12 bg-cyan"></div>
+
         <div className="flex flex-col items-center text-center">
-          <img
-            src="./svgs/icon-fully-customizable.svg"
-            alt="Fully Customizable Illustration"
-          />
-          <h2 className="text-xl font-bold text-verydarkviolet mb-5">
+          <div className="flex justify-center items-center bg-darkviolet w-20 h-20 mb-8 rounded-full">
+            <img
+              src="./svgs/icon-fully-customizable.svg"
+              alt="Fully Customizable Illustration"
+            />
+          </div>
+          <h2 className="text-2xl font-bold text-verydarkviolet mb-5">
             Fully Customizable
           </h2>
-          <p className="text-grayishviolet font-medium">
+          <p className="w-[94%] text-grayishviolet font-medium">
             Improve brand awareness and content discoverability through
             customizable links, supercharging audience engagement.
           </p>
@@ -60,7 +75,7 @@ const InfoSection = () => {
 
   const DesktopInfoSection = () => (
     <div className="lg:xy-p mxy-p">
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center my-4">
         <h1 className="text-4xl font-bold text-verydarkviolet mb-4">
           Advanced Statistics
         </h1>
@@ -71,7 +86,7 @@ const InfoSection = () => {
       </div>
 
       <div className="relative flex flex-row justify-center">
-        <div className="max-h-[320px] w-[380px] bg-white p-6 rounded-lg shadow-lg">
+        <div className="max-h-[320px] w-[360px] bg-white p-6 rounded-lg shadow-lg">
           <div className="flex justify-center items-center bg-darkviolet w-20 h-20 mb-8 rounded-full">
             <img
               src="./svgs/icon-brand-recognition.svg"
@@ -88,9 +103,9 @@ const InfoSection = () => {
         </div>
 
         {/* Cyan Bar */}
-        <div className="w-16 h-2 bg-cyan self-center transform translate-y-12"></div>
+        <div className="w-8 h-2 bg-cyan self-center transform translate-y-12"></div>
 
-        <div className="max-h-[320px] w-[380px] bg-white p-6 rounded-lg shadow-lg mt-12">
+        <div className="max-h-[320px] w-[360px] bg-white p-6 rounded-lg shadow-lg mt-12">
           <div className="flex justify-center items-center bg-darkviolet w-20 h-20 mb-8 rounded-full">
             <img
               src="./svgs/icon-detailed-records.svg"
@@ -107,9 +122,9 @@ const InfoSection = () => {
         </div>
 
         {/* Cyan Bar */}
-        <div className="w-16 h-2 bg-cyan self-center transform translate-y-12"></div>
+        <div className="w-8 h-2 bg-cyan self-center transform translate-y-12"></div>
 
-        <div className="max-h-[320px] w-[380px] bg-white p-6 rounded-lg shadow-lg mt-24">
+        <div className="max-h-[320px] w-[360px] bg-white p-6 rounded-lg shadow-lg mt-24">
           <div className="flex justify-center items-center bg-darkviolet w-20 h-20 mb-8 rounded-full">
             <img
               src="./svgs/icon-fully-customizable.svg"
